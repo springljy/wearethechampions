09984d22f3146903d22263009854452e61ce6747
Spring <springljy12@gmail.com>
2024-09-16 14:13:39 +0800

    Initial commit

---
+++ src/main/java/com/example/WeAreTheChampions/WeAreTheChampionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.WeAreTheChampions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeAreTheChampionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeAreTheChampionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WeAreTheChampions/WeAreTheChampionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WeAreTheChampions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeAreTheChampionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
